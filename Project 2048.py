95e3e5c7ce4933d07a67957c977f6cf3b1a0d17b
chshshsh <44385295+chshshsh@users.noreply.github.com>
2018-12-02 12:57:42 -0500

    Update Project 2048.py
    complete basic logic for move left as base movement

--- Project 2048.py
+++ Project 2048.py
@@ -65,7 +65,35 @@ class Grid(object):
             new_cells.append(row[::-1])
         self.cells = new_cells  
                         
-    
+    def move_row_left(self, row):
+        #move numbers to the most left nonempty cells and fill up right cells with 0  
+        def tighten(row):
+            new_row = []
+            for i in row:
+                if i != 0:
+                    new_row.append(i)
+            for i in range(len(row) - len(new_row)):
+                new_row.append(0)
+            return new_row
+        
+        #merge two same numbers in adjacent cells and cumulate score 
+        def merge(row):
+            pair = False
+            new_row = []
+            for i in range(len(row)):
+                if pair:
+                    new_row.append(row[i] + row[i])
+                    self.parent.score += 2 * row[i]
+                    pair = False
+                else:
+                    if i < len(row) - 1 and row[i] == row[i + 1]:
+                        pair = True
+                        new_row.append(0)
+                    else:
+                        new_row.append(row[i])
+            return new_row
+        return tighten(merge(tighten(row)))
+
 
 class Screen(object):
     menu1 = '(W)up (S)down (A)left (D)right'
